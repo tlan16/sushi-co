5a0a5262391f73ec9e37b3551a02a8029e9c666d
helin16 <helin16@192.168.200.202>
2015-11-12 17:32:41 +1100

    bug fixing

--- web/protected/layout/Header/Header.php
+++ web/protected/layout/Header/Header.php
@@ -29,7 +29,7 @@ class Header extends TTemplateControl
 	
 	public function getStoreChangeBtn()
 	{
-		if(UserAccountInfo::countByCriteria('active = 1 and entityName = :eName and typeId = :typeId and userAccountId = :uid', array('eName' => 'Role', 'typeId' => UserAccountInfoType::ID_ROLE, 'uid' => $userAccount->getId())) > 1) {
+		if(UserAccountInfo::countByCriteria('active = 1 and entityName = :eName and typeId = :typeId and userAccountId = :uid', array('eName' => 'Role', 'typeId' => UserAccountInfoType::ID_ROLE, 'uid' => Core::getUser()->getId())) > 1) {
 			return '<li><a href="/store/switch.html">Change Store</a></li>';
 		}
 		return '';
