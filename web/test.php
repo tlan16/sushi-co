62c492b6c4c230d65009db9d57e5baf1285863e5
helin16 <helin16@helin16-VirtualBox>
2015-12-03 12:17:54 +1100

    more debug

--- web/test.php
+++ web/test.php
@@ -1,3 +1,23 @@
 <?php
-require 'bootstrap.php';
-EmailSender::sendEmail('test@test.com', 'helin16@gmail.com', 'test', 'test');
+$mail = new PHPMailer();
+$mail->IsSMTP();
+$mail->CharSet = 'UTF-8';
+$mail->Host = "smtp.gmail.com";
+$mail->SMTPAuth= true;
+$mail->Port = 587; // Or 587
+$mail->Username= 'ljddfoodsupply@outlook.com';
+$mail->Password= '24JubileeStreet';
+$mail->SMTPSecure = 'tls';
+$mail->From = 'noply@sushiandco.com';
+$mail->FromName= 'SushiAndCo';
+$mail->isHTML(true);
+$mail->Subject = 'test';
+$mail->Body = 'test';
+$mail->addAddress('helin16@gmail.com');
+$mail->SMTPDebug = 2;
+
+if(!$mail->send()){
+	echo "Mailer Error: " . $mail->ErrorInfo;
+}else{
+	echo "E-Mail has been sent";
+}
