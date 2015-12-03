a6ddc996ac6393e1d8d5df8b937d27e4619e2b61
helin16 <helin16@helin16-VirtualBox>
2015-12-03 12:24:16 +1100

    testing

--- web/test.php
+++ web/test.php
@@ -9,7 +9,7 @@ $mail->Port = 587; // Or 587
 $mail->Username= 'ljddfoodsupply@outlook.com';
 $mail->Password= '24JubileeStreet';
 $mail->SMTPSecure = 'tls';
-$mail->From = 'noply@sushiandco.com';
+$mail->From = 'ljddfoodsupply@outlook.com';
 $mail->FromName= 'SushiAndCo';
 $mail->isHTML(true);
 $mail->Subject = 'test';
